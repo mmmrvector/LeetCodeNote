62a84c64d0ec56f1d5ecd856141d61c05b1853c7
vector <921629562@qq.com>
2021-08-11 01:05:02 +0800

    No. 74

---
+++ LeetCodeNote.md
@@ -0,0 +1,10 @@
+#### 2020-3-16
+
+**No. 38 Count and Say**
+
+属于Easy难度，主要用于再次熟悉Java语法及LeetCode，难度不大。
+
+**No.39 Combination Sum**
+
+属于Medium难度，难度不大，主要熟悉Java List相关语法。这题上主要遇到的问题在于如何不重复（排序）以及各种边界条件。
+
---
+++ src/LeetCode.java
@@ -0,0 +1,7 @@
+public class LeetCode {
+    public static void main(String args[]) {
+        Solution74 solution = new Solution74();
+        int matrix[][] = new int [][] {{1,2,3},{4,5,6}, {7,8,9}};
+        System.out.println(solution.searchMatrix(matrix, 10));
+    }
+}
