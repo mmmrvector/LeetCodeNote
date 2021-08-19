1e2daf7544407fd764334fbab78735650ca0bcda
mmmrvector <921629562@qq.com>
2021-08-19 16:12:58 +0800

    STR: No.44 hard, need review

--- String/Solution44.py
+++ String/Solution44.py
@@ -6,33 +6,39 @@ class Solution(object):
         :rtype: bool
         """
         import copy
-        new_p = ""
-        for i, pp in enumerate(p):
-            if i == 0 or (p[i] != p[i - 1]) or(p[i] == p[i - 1] and p[i] != "*"):
-                new_p += pp
-        p = new_p
-        memo = {}
-        def match(s, p):
-            if len(s) == 0:
-                if p == "*" or p == "":
-                    return True
+        # new_p = ""
+        # for i, pp in enumerate(p):
+        #     if i == 0 or (p[i] != p[i - 1]) or(p[i] == p[i - 1] and p[i] != "*"):
+        #         new_p += pp
+        # p = new_p
+        # memo = {}
+        m = len(s)
+        n = len(p)
+        dp = [[False for _ in range(n + 1)] for __ in range(m + 1)]
+        dp[0][0] = True
+        for i in range(1, m):
+            dp[i][0] = False
+        for i in range(m + 1):
+            for j in range(1, n + 1):
+                if i == 0:
+                    if p[j - 1] == "*":
+                        dp[i][j] = dp[i][j - 1]
+                    else:
+                        dp[i][j] = False
                 else:
-                    return False
-            if len(p) == 0:
-                return False
-            if s +'-' + p in memo:
-                return memo[s +'-' + p]
+                    if p[j - 1] == s[i - 1] or p[j - 1] == '?':
+                        dp[i][j] = dp[i - 1][j - 1]
+                        continue
+                    if p[j - 1] == '*':
+                        if dp[i - 1][j] or dp[i][j - 1]:
+                            dp[i][j] = True
+                    else:
+                        dp[i][j] = False
 
-            if p[-1] == s[-1] or p[-1] == "?":
-                return self.isMatch(s[:-1], p[:-1])
-            if p[-1] != "*":
-                return False
-            for i in range(len(s) + 1):
-                if self.isMatch(s[:i], p[:-1]):
-                    memo[s + '-' + p] = True
-                    return True
-            memo[s + '-' + p] = False
-            return False
-        return match(s, p)
+        return dp[m][n]
+
+
+
 s = Solution()
-print(s.isMatch("abcabczzzde","*abc???de*"))
+#print(s.isMatch("abcabczzzde","*abc???de*"))
+print(s.isMatch("baaaababbbaabbbbabbababaababaaabaababbbaabaabbbbaabbbbbbaabaabbababaaaaaaaabbbaaabbbababbbbbabbbbabbbbabbaabaababababbbababbbbbbbaaaaabbbbabbbbbaabbbaaaabaaabbabbabaabbbbabbbabbbaababbabbaaaababbababa","*bb*abb*a**ba**aba*b*bbb*abbaaa*bb*b**a*b*b**a**abb***ab*b**b*b*a******a*a*babaa*bab*a*b****bb*babb*baa"))
