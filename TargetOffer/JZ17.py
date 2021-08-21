80fbb0a775f2c8132f0878082ee625b6f0e54f7a
mmmrvector <921629562@qq.com>
2021-08-21 15:20:18 +0800

    TreeL JZ17

--- TargetOffer/JZ17.py
+++ TargetOffer/JZ17.py
@@ -7,3 +7,24 @@
 class Solution:
     def HasSubtree(self, pRoot1, pRoot2):
         # write code here
+        if pRoot1 is None or pRoot2 is None:
+            return False
+        def isSame(p1, p2):
+            if p2 is None:
+                return True
+            if p1 is None:
+                return False
+            if p1.val == p2.val:
+                return isSame(p1.left, p2.left) and isSame(p1.right, p2.right)
+            else:
+                return False
+        q = [pRoot1]
+        while len(q) != 0:
+            root = q.pop(0)
+            if isSame(root, pRoot2):
+                return True
+            if root.left is not None:
+                q.append(root.left)
+            if root.right is not None:
+                q.append(root.right)
+        return False
