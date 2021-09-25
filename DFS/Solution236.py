5dabe5b5561bed22a1da139cb91f5350e3b1552d
mmmrvector <921629562@qq.com>
2021-09-25 18:06:36 +0800

    DFS

---
+++ DFS/Solution236.py
@@ -0,0 +1,33 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def __init__(self):
+        self.pf = None
+        self.qf = None
+    def lowestCommonAncestor(self, root, p, q):
+        """
+        :type root: TreeNode
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: TreeNode
+        """
+        if not root:
+            return None
+        if root == p:
+            return p
+        if root == q:
+            return q
+        l = self.lowestCommonAncestor(root.left, p, q)
+        r = self.lowestCommonAncestor(root.right, p, q)
+        if l and r:
+            return root
+        else:
+            if l:
+                return l
+            else:
+                return r
